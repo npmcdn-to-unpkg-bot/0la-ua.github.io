935109af8af7aac39d3eb524ed0edcb33c140ef6
0la-ua <olga.lsk@gmail.com>
2016-04-06 15:50:56 +0300

    array of pokemons with history

--- pokemon/assets/lib/sjs.js
+++ pokemon/assets/lib/sjs.js
@@ -3,45 +3,51 @@ var imgURL="http://pokeapi.co/media/img/",
 	imgURL1="url('",
 	imgURL2="') no-repeat 50% 50%",
 	URI="http://pokeapi.co/api/v1/pokemon/?limit=12";	
-arrP=[], nextURL=URI;
+arrP=[[],[]], nextURL=URI;
+var getN=function(s){
+	for(var i=s.length-1;i>0;) if(!isNaN(s[i])) --i; else return i+1;
+};
 function getPokemons12() {
-	$.getJSON(nextURL, function(data) {		 
+	console.log("num="+num);
+	$.getJSON(nextURL, function(data) {				
 	 	 for (var i=0; i<data.objects.length; i++){
 	 	 	//console.log(data.objects[i]);  getall
-	 	 	arrP[i]={};	 	 		 	 	
-	 		arrP[i].idP=data.objects[i].pkdx_id;
-	 		arrP[i].nameP=data.objects[i].name;
-	 		arrP[i].urlP=data.objects[i].resource_uri;
-	 		arrP[i].powerP=data.objects[i].types;
-	 		arrP[i].attack=data.objects[i].attack;
-	 		arrP[i].defense=data.objects[i].defense;
-	 		arrP[i].hp=data.objects[i].hp;
-	 		arrP[i].sp_atk=data.objects[i].sp_atk;
-	 		arrP[i].sp_def=data.objects[i].sp_def;
-	 		arrP[i].speed=data.objects[i].speed;
-	 		arrP[i].weight=data.objects[i].weight;
-	 		arrP[i].total_m=data.objects[i].moves.length;
+	 	 	arrP[num][i]={};	 	 		 	 	
+	 		arrP[num][i].idP=data.objects[i].pkdx_id;
+	 		arrP[num][i].nameP=data.objects[i].name;
+	 		arrP[num][i].urlP=data.objects[i].resource_uri;
+	 		arrP[num][i].powerP=data.objects[i].types;
+	 		arrP[num][i].attack=data.objects[i].attack;
+	 		arrP[num][i].defense=data.objects[i].defense;
+	 		arrP[num][i].hp=data.objects[i].hp;
+	 		arrP[num][i].sp_atk=data.objects[i].sp_atk;
+	 		arrP[num][i].sp_def=data.objects[i].sp_def;
+	 		arrP[num][i].speed=data.objects[i].speed;
+	 		arrP[num][i].weight=data.objects[i].weight;
+	 		arrP[num][i].total_m=data.objects[i].moves.length;
 	 	 };
 		for (var i=0; i<singleP.length;i++){
-			singleP[i].children[1].innerHTML=arrP[i].nameP;
-			document.getElementsByClassName("img"+(i+1))[0].style.background=imgURL1+imgURL+arrP[i].idP+".png"+imgURL2;
-			for(var j=0;j<arrP[i].powerP.length;j++) addEl(arrP[i].powerP[j].name,"single"+(i+1));
+			singleP[i].children[1].innerHTML=arrP[num][i].nameP;
+			document.getElementsByClassName("img"+(i+1))[0].style.background=imgURL1+imgURL+arrP[num][i].idP+".png"+imgURL2;
+			for(var j=0;j<arrP[num][i].powerP.length;j++) addEl(arrP[num][i].powerP[j].name,"single"+(i+1));
 		};
 		nextURL="http://pokeapi.co"+data.meta.next;
 	});
 };
 if (nextURL===null) nextURL=URI;
+num=nextURL.substring(getN(nextURL),nextURL.length)/12-1;
 getPokemons12(nextURL);
 card=0;
 document.getElementsByClassName("col-1-2")[0].onclick = function(){
 	var b;
 	if (event.target.id==="getPokemons") {
-		arrP=[];
+		num=nextURL.substring(getN(nextURL),nextURL.length)/12;
+		arrP[num]=[[],[]];
 		//document.getElementsByClassName("infobox")[0].style.display="none";
 		do{
 			b=document.getElementsByTagName("button");
 			b[0].remove();
-		}while (b.length>1);
+		}while (b.length>1);		
 		getPokemons12(nextURL);
 	}
 	else {
@@ -61,7 +67,7 @@ function singlePokemon(event){
 		card=0;
 	}
 	else card=k;
-	showinfo(arrP[k-1]);
+	showinfo(arrP[num][k-1]);
 }
 function addEl(powername,classname){
 	var b=document.createElement("button"),t=document.createTextNode(powername);
