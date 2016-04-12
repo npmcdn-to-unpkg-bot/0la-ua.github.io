a45442f67d96ea3a712194e738cab759bf74fffd
0la-ua <olga.lsk@gmail.com>
2016-04-12 11:32:24 +0300

    adding info (not rewriting)

--- pokemon/assets/lib/sjs.js
+++ pokemon/assets/lib/sjs.js
@@ -1,4 +1,6 @@
-var singleP = document.getElementsByClassName("single");
+
+//var singleP = document.getElementsByClassName("single");
+var singleP_row=document.getElementById("row1");
 var imgURL="http://pokeapi.co/media/img/",
 	imgURL1="url('",
 	imgURL2="') no-repeat 50% 50%",
@@ -25,12 +27,39 @@ function getPokemons12() {
 	 		arrP[num][i].speed=data.objects[i].speed;
 	 		arrP[num][i].weight=data.objects[i].weight;
 	 		arrP[num][i].total_m=data.objects[i].moves.length;
+
+	 		//create card
+	 		var singleP=document.createElement('div');//<div class="col-1-3">
+	 		singleP.className="col-1-3";
+		 	var divSingle=document.createElement('div');//<div class="single" id="single1">
+		 	divSingle.className = "single";
+		 	divSingle.id="single"+(num*12+i);
+		 	divImg=document.createElement('div');
+		 	divImg.className="imgg img"+(num*12+i);//<div class="imgg img1"></div>
+		 	divImg.style.background=imgURL1+imgURL+arrP[num][i].idP+".png"+imgURL2;
+		 	divSingle.appendChild(divImg);//added image	 	
+		 	var p_name=document.createElement('p');//<p id="name1" class="name">Name</p>
+			var p_node = document.createTextNode(arrP[num][i].nameP);
+			p_name.appendChild(p_node);//added name
+			p_name.id="name"+(num*12+i);
+			p_name.className="name";
+			divSingle.appendChild(p_name);//buttons
+			for (var j=0;j<arrP[num][i].powerP.length;j++) {
+		 		var b=document.createElement("button"),
+		 		t=document.createTextNode(arrP[num][i].powerP[j].name);
+				b.appendChild(t);
+				b.className="but-pow "+arrP[num][i].powerP[j].name;
+				divSingle.appendChild(b);//added buttons
+			};			
+			singleP.appendChild(divSingle);	
+			singleP_row.appendChild(singleP);
+		 	 
 	 	 };
-		for (var i=0; i<singleP.length;i++){
-			singleP[i].children[1].innerHTML=arrP[num][i].nameP;
-			document.getElementsByClassName("img"+(i+1))[0].style.background=imgURL1+imgURL+arrP[num][i].idP+".png"+imgURL2;
-			for(var j=0;j<arrP[num][i].powerP.length;j++) addEl(arrP[num][i].powerP[j].name,"single"+(i+1));
-		};
+		// for (var i=0; i<singleP.length;i++){
+		// 	//singleP[i].children[1].innerHTML=arrP[num][i].nameP;
+		// 	document.getElementsByClassName("img"+(i+1))[0].style.background=imgURL1+imgURL+arrP[num][i].idP+".png"+imgURL2;
+		// 	for(var j=0;j<arrP[num][i].powerP.length;j++) addEl(arrP[num][i].powerP[j].name,"single"+(i+1));
+		// };
 		nextURL="http://pokeapi.co"+data.meta.next;
 	});
 };
@@ -44,10 +73,10 @@ document.getElementsByClassName("col-1-2")[0].onclick = function(){
 		num=nextURL.substring(getN(nextURL),nextURL.length)/12;
 		arrP[num]=[[],[]];
 		//document.getElementsByClassName("infobox")[0].style.display="none";
-		do{
-			b=document.getElementsByTagName("button");
-			b[0].remove();
-		}while (b.length>1);		
+		// do{
+		// 	b=document.getElementsByTagName("button");
+		// 	b[0].remove();
+		// }while (b.length>1);		
 		getPokemons12(nextURL);
 	}
 	else {
@@ -57,17 +86,25 @@ document.getElementsByClassName("col-1-2")[0].onclick = function(){
 (function(){
 })();
 function singlePokemon(event){
-	var k=0;	
-	k=index(event.target.id)||index(event.target.parentElement.id);
+	var k=0,shown=0,row=0;
+	//console.log("t.id "+event.target.id);
+	//console.log("p.id "+event.target.parentElement.id);
+	//console.log("t clName "+event.target.className);
+	// console.log("e.targ "+event.target.className);
+	// console.log("e.parnt "+event.target.parentElement.className);
+	// console.log("e.cTarg "+event.currentTarget.className);
+	shown=index(event.target.id)||index(event.target.parentElement.id);
+	row=Math.floor(shown/12);
+	k=shown-row*12;
 	if (card===0) {		
-		document.getElementsByClassName("infobox")[0].style.display="block";card=k;
+		document.getElementsByClassName("infobox")[0].style.display="block";card=shown;
 	}
-	else if (card===k){
+	else if (card===shown){
 		document.getElementsByClassName("infobox")[0].style.display="none";
 		card=0;
 	}
-	else card=k;
-	showinfo(arrP[num][k-1]);
+	else card=shown;
+	showinfo(arrP[row][k]);
 }
 function addEl(powername,classname){
 	var b=document.createElement("button"),t=document.createTextNode(powername);
